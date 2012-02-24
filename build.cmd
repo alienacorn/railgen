abc3b1db927f307e3ddc0fb1b91d76bec4f637a0
George Saunders <georgesaunders@gmail.com>
2012-02-23 21:51:51 -0500

    Changed rev 6 to rev 7.

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-ruby railgen.rb reddit-pve-6-rails.yaml
-ruby railgen.rb -t templates/subwaymap.haml reddit-pve-6-rails.yaml
-python generate-nodes.py reddit-pve-6-rails.yaml pve-6-rails-connection.js  --javascript --quiet
-ruby railgen.rb reddit-pve-6-rails.yaml -t templates/dynmap.haml -o userscript/dynmap.js
+ruby railgen.rb reddit-pve-7-rails.yaml
+ruby railgen.rb -t templates/subwaymap.haml reddit-pve-7-rails.yaml
+python generate-nodes.py reddit-pve-7-rails.yaml pve-7-rails-connection.js  --javascript --quiet
+ruby railgen.rb reddit-pve-7-rails.yaml -t templates/dynmap.haml -o userscript/dynmap.js
